03fdef2e1db8157c51b2ec97b7c9d14ae72c2775
yingkiongstore <154452566+yingkiongstore@users.noreply.github.com>
2023-12-20 23:05:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Yingkiong Store coming soon.
+        </p>
+        <p>
+          We are working on it
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
